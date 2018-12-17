6fa9f5e2255c223aafeffa0774151b81c6a659a3
00tanker00 <brandonjolson@gmail.com>
2018-12-17 11:34:33 +0700

    commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+extern crate unicorn_hat_hd;
+extern crate rgb;
+
+use unicorn_hat_hd::UnicornHatHd;
+
+const RED: rgb::RGB8 = rgb::RGB8 {r: 255, g: 0, b: 0};
+const CLEAR: rgb::RGB8 = rgb::RGB8 {r: 0, g: 0, b: 0};
+const BLUE: rgb::RGB8 = rgb::RGB8 {r: 0, g: 0, b: 255};
+
+pub fn main() {
+    let mut hat_hd = UnicornHatHd::default();
+    loop {
+        for y in 0..16 {
+            for x in 0..16 {
+                hat_hd.set_pixel(x, y, RED);
+                hat_hd.display().unwrap();
+                hat_hd.set_pixel(x, y, BLUE);
+                hat_hd.display().unwrap();
+                hat_hd.set_pixel(x, y, CLEAR);
+            }
+        }
+    }
+}
