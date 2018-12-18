c19d46660c3c70198ffffe308b42251122648ae1
00tanker00 <brandonjolson@gmail.com>
2018-12-18 11:03:28 +0700

    :|

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use unicorn_hat_hd::UnicornHatHd;
 
 const RED: rgb::RGB8 = rgb::RGB8 {r: 255, g: 0, b: 0};
 const CLEAR: rgb::RGB8 = rgb::RGB8 {r: 0, g: 0, b: 0};
-const BLUE: rgb::RGB8 = rgb::RGB8 {r: 0, g: 0, b: 255};
+//const BLUE: rgb::RGB8 = rgb::RGB8 {r: 0, g: 0, b: 255};
+const GREEN: rgb::RGB8 = rgb::RGB8 {r: 0, g: 255, b: 0};
 
 pub fn main() {
     let mut hat_hd = UnicornHatHd::default();
@@ -14,7 +15,9 @@ pub fn main() {
             for x in 0..16 {
                 hat_hd.set_pixel(x, y, RED);
                 hat_hd.display().unwrap();
-                hat_hd.set_pixel(x, y, BLUE);
+                //hat_hd.set_pixel(x, y, BLUE);
+                //hat_hd.display().unwrap();
+                hat_hd.set_pixel(x, y, GREEN);
                 hat_hd.display().unwrap();
                 hat_hd.set_pixel(x, y, CLEAR);
             }
